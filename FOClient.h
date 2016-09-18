e85e24db1127bbc221f6bce852a3e4c7ab4dc92e
EnglishBreakfastTea <assam.ceylon.kenya@gmail.com>
2016-09-19 00:54:16 +0200

    HexManager, Critter, Object structs. Attack critter under cursor.

--- FOClient.h
+++ FOClient.h
@@ -3,14 +3,29 @@
 
 #include <cstdint>
 
-struct FOClient;
+struct HexManager {};
+
+struct FOClient {
+    uint32_t _padding[8];
+    HexManager hexManager; // at 0x20
+};
+
+struct Object; // from a Xander Root lying on the ground to a standing Wagon
+struct Critter {
+    uint32_t critterId;
+};
 
 using FastTick = uint32_t (_stdcall *)();
 using IsAction = uint32_t (_thiscall *)(FOClient*, uint32_t);
 using SetAction = void (_thiscall *)(FOClient*, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t);
+using GetSmthPixel = void (_thiscall *)(HexManager*, uint32_t mouseX, uint32_t mouseY, Object*&, Critter*&);
 
 uint32_t const FastTickAddr = 0x00538940;
 uint32_t const IsActionAddr = 0x00472110;
 uint32_t const SetActionAddr = 0x0048DDE0;
+uint32_t const GetSmthPixelAddr = 0x00504C40;
+
+uint32_t const MouseXAddr = 0x008520C4;
+uint32_t const MouseYAddr = 0x008520C8;
 
 #endif //_FOCLIENT_H
--- mainLoop.cpp
+++ mainLoop.cpp
@@ -17,6 +17,10 @@ int const mqMaxMsgSize = 100;
 auto fastTick = reinterpret_cast<FastTick>(FastTickAddr);
 auto isAction = reinterpret_cast<IsAction>(IsActionAddr);
 auto setAction = reinterpret_cast<SetAction>(SetActionAddr);
+auto getSmthPixel = reinterpret_cast<GetSmthPixel>(GetSmthPixelAddr);
+
+auto mouseX = reinterpret_cast<uint32_t*>(MouseXAddr);
+auto mouseY = reinterpret_cast<uint32_t*>(MouseYAddr);
 
 void move(FOClient* client, uint32_t x, uint32_t y)
 {
@@ -26,6 +30,13 @@ void move(FOClient* client, uint32_t x, uint32_t y)
     auto run = 1;
     // reverse x, y?
     setAction(client, ACTION_MOVE, x, y, run, 0, !moving, time);
+}
+
+void attack(FOClient* client, uint32_t critterId)
+{
+    auto ACTION_ATTACK = 6u;
+    auto NO_AIM = 0u;
+    setAction(client, ACTION_ATTACK, 0, 0x3e9, 2, critterId, NO_AIM, 0);
 }
 
 struct State {
@@ -76,14 +87,43 @@ void mainLoop(FOClient* client)
     }
 
     auto msg = getMessage(*state);
-    if (msg) {
-        printf("received message: %s\n", msg->data());
+    if (!msg) {
+        return;
+    }
 
+    printf("received message: %s\n", msg->data());
+
+    {
         uint32_t x, y;
         if (sscanf(msg->data(), "move %u %u", &x, &y) == 2) {
             printf("a move message\n");
             move(client, x, y);
             printf("called move\n");
+            return;
+        }
+    }
+
+    {
+        if (*msg == "attack") {
+            printf("attack message\n");
+
+            Object* obj = nullptr;
+            Critter* critt = nullptr;
+            getSmthPixel(&client->hexManager, *mouseX, *mouseY, obj, critt);
+
+            if (obj) {
+                printf("mouse points at an object\n");
+            }
+
+            if (critt) {
+                printf("mouse points at a critter\n");
+            }
+
+            if (!critt) {
+                return;
+            }
+
+            attack(client, critt->critterId);
         }
     }
 }
