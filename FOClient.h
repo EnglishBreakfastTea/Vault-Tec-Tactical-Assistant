381285ebdf5896c7b3e3957d5221bdc5b21544f6
EnglishBreakfastTea <assam.ceylon.kenya@gmail.com>
2016-09-19 18:30:45 +0200

    Fix attack. Check critter neighbourhood. ComplexAction class.

--- FOClient.h
+++ FOClient.h
@@ -3,27 +3,55 @@
 
 #include <cstdint>
 
-struct HexManager {};
+struct Item {
+    uint32_t itemId;
+};
 
-struct FOClient {
-    uint32_t _padding[8];
-    HexManager hexManager; // at 0x20
+struct Hand { // I don't know what this is, but it points to the held item, so I called it Hand.
+    uint32_t handItemId; // ??
+    Item* heldItem;
 };
 
 struct Object; // from a Xander Root lying on the ground to a standing Wagon
+
 struct Critter {
     uint32_t critterId;
+    uint16_t _padding1;
+    uint16_t x;
+    uint16_t y;
+    uint16_t _padding2;
+    // 4 + 2 + 2 + 2 + 2 = 12 = 4 * 3
+    uint32_t _padding3[1064]; // 4256
+    // 4256 + 12 = 4268 = 4 * 1067
+    Hand* hand;
+};
+
+struct HexManager {
+    uint32_t _padding[72];
+    uint32_t playerCritterId; // could be inside FOClient
+    // 73 * 4 = 292
+};
+
+struct FOClient {
+    uint32_t _padding1[8]; // 8 * 4 = 32
+    HexManager hexManager; // at 0x20
+    // 32 + 292 = 324 = 81 * 4
+    uint32_t _padding2[11685]; // 46740
+    // 324 + 46740 = 47064 = 4 * 11766
+    Critter* playerCritter; // at 0xb7d8
 };
 
 using FastTick = uint32_t (_stdcall *)();
 using IsAction = uint32_t (_thiscall *)(FOClient*, uint32_t);
 using SetAction = void (_thiscall *)(FOClient*, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t);
 using GetSmthPixel = void (_thiscall *)(HexManager*, uint32_t mouseX, uint32_t mouseY, Object*&, Critter*&);
+using GetCritter = Critter* (_thiscall *)(HexManager*, uint32_t critterId);
 
 uint32_t const FastTickAddr = 0x00538940;
 uint32_t const IsActionAddr = 0x00472110;
 uint32_t const SetActionAddr = 0x0048DDE0;
 uint32_t const GetSmthPixelAddr = 0x00504C40;
+uint32_t const GetCritterAddr = 0x004800C0;
 
 uint32_t const MouseXAddr = 0x008520C4;
 uint32_t const MouseYAddr = 0x008520C8;
--- mainLoop.cpp
+++ mainLoop.cpp
@@ -18,6 +18,7 @@ auto fastTick = reinterpret_cast<FastTick>(FastTickAddr);
 auto isAction = reinterpret_cast<IsAction>(IsActionAddr);
 auto setAction = reinterpret_cast<SetAction>(SetActionAddr);
 auto getSmthPixel = reinterpret_cast<GetSmthPixel>(GetSmthPixelAddr);
+auto getCritter = reinterpret_cast<GetCritter>(GetCritterAddr);
 
 auto mouseX = reinterpret_cast<uint32_t*>(MouseXAddr);
 auto mouseY = reinterpret_cast<uint32_t*>(MouseYAddr);
@@ -27,7 +28,7 @@ void move(FOClient* client, uint32_t x, uint32_t y)
     auto time = fastTick();
     auto moving = isAction(client, 1);
     auto ACTION_MOVE = 1u;
-    auto run = 1;
+    auto run = 1u;
     // reverse x, y?
     setAction(client, ACTION_MOVE, x, y, run, 0, !moving, time);
 }
@@ -36,13 +37,103 @@ void attack(FOClient* client, uint32_t critterId)
 {
     auto ACTION_ATTACK = 6u;
     auto NO_AIM = 0u;
-    setAction(client, ACTION_ATTACK, 0, 0x3e9, 2, critterId, NO_AIM, 0);
+
+    auto playerCritter = client->playerCritter;
+    if (!playerCritter) {
+        printf("no player critter!\n");
+        return;
+    }
+
+    auto hand = playerCritter->hand;
+    if (!hand) {
+        printf("what, no hand\n");
+        return;
+    }
+
+    auto heldItem = hand->heldItem;
+    if (!heldItem) {
+        // an empty hand should also be an item
+        printf("no hand item\n");
+        return;
+    }
+
+    setAction(client, ACTION_ATTACK, hand->handItemId, heldItem->itemId, 2, critterId, NO_AIM, 0);
 }
+
+Critter* critterUnderMouse(HexManager* hexManager)
+{
+    Object* obj = nullptr;
+    Critter* critt = nullptr;
+    getSmthPixel(hexManager, *mouseX, *mouseY, obj, critt);
+
+    if (!critt) {
+        return nullptr;
+    }
+
+    return critt;
+}
+
+bool crittersNeighbours(HexManager* hexManager, uint32_t id1, uint32_t id2)
+{
+    auto static evenXDeltas = std::array<std::pair<int, int>, 6>
+            {{{1,0}, {0, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}}};
+    auto static oddXDeltas = std::array<std::pair<int, int>, 6>
+            {{{1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {0, 1}, {1, 0}}};
+
+    auto critter1 = getCritter(hexManager, id1);
+    auto critter2 = getCritter(hexManager, id2);
+
+    printf("critter1: %u, %u; critter2: %u, %u\n", critter1->x, critter1-> y, critter2->x, critter2->y);
+
+    if (critter1->x % 2 == 0) {
+        return std::any_of(std::begin(evenXDeltas), std::end(evenXDeltas), [=](auto delta) {
+            return critter1->x + delta.first == critter2->x
+                && critter1->y + delta.second == critter2->y;
+        });
+    }
+
+    return std::any_of(std::begin(oddXDeltas), std::end(oddXDeltas), [=](auto delta) {
+        return critter1->x + delta.first == critter2->x
+            && critter1->y + delta.second == critter2->y;
+    });
+}
+
+bool playerNear(HexManager* hexManager, uint32_t critterId)
+{
+    auto playerCritterId = hexManager->playerCritterId;
+    return crittersNeighbours(hexManager, critterId, playerCritterId);
+}
+
+/* A ComplexAction is a (non-trivial) algorithm that performs (multiple) FOnline actions.
+ * Constructing a ComplexAction initiates the sequence of actions to be performed, starting with the next frame.
+ * Destructing a ComplexAction stops any new actions in the sequence from being performed in the upcoming frames.
+ * The constructor must not start any action. That comes later in the frame. */
+class ComplexAction {
+public:
+    /* Returns true when the action is finished. */
+    virtual bool frame(FOClient*) = 0;
+};
+
+class HexAttack : public ComplexAction {
+public:
+    HexAttack(uint32_t critterId)
+        : critterId(critterId)
+    {
+    }
+
+    bool frame(FOClient* client) override
+    {
+        return true;
+    }
+
+private:
+    uint32_t critterId;
+};
 
 struct State {
     State()
         : mq(create_only, mqName, 100, mqMaxMsgSize)
-        , stuff(5)
+        , complexAction(nullptr)
     {
     }
 
@@ -51,8 +142,8 @@ struct State {
         message_queue::remove(mqName);
     }
 
-    int stuff;
     message_queue mq;
+    std::unique_ptr<ComplexAction> complexAction;
 };
 
 optional<std::string> getMessage(State& state)
@@ -71,7 +162,8 @@ optional<std::string> getMessage(State& state)
 
 void setup(std::unique_ptr<State>& state)
 {
-    message_queue::remove(mqName); //TODO?
+    // TODO: smarter handling of an existing queue.
+    message_queue::remove(mqName);
     state = std::make_unique<State>();
 }
 
@@ -83,16 +175,24 @@ void mainLoop(FOClient* client)
         setup(state);
 
         printf("\nFOClient address: %d\n", reinterpret_cast<uint32_t>(client));
-        printf("state stuff: %d\n", state->stuff);
     }
 
     auto msg = getMessage(*state);
+
     if (!msg) {
+        if (state->complexAction) {
+            state->complexAction->frame(client);
+        }
+
         return;
     }
 
+    // Any received messages stop the current ComplexAction. For now...
+    state->complexAction = nullptr;
+
     printf("received message: %s\n", msg->data());
 
+    // TODO: smarter message handling dispatch.
     {
         uint32_t x, y;
         if (sscanf(msg->data(), "move %u %u", &x, &y) == 2) {
@@ -107,23 +207,45 @@ void mainLoop(FOClient* client)
         if (*msg == "attack") {
             printf("attack message\n");
 
-            Object* obj = nullptr;
-            Critter* critt = nullptr;
-            getSmthPixel(&client->hexManager, *mouseX, *mouseY, obj, critt);
-
-            if (obj) {
-                printf("mouse points at an object\n");
-            }
-
-            if (critt) {
-                printf("mouse points at a critter\n");
-            }
-
-            if (!critt) {
+            auto critter = critterUnderMouse(&client->hexManager);
+            if (!critter) {
                 return;
             }
 
-            attack(client, critt->critterId);
+            attack(client, critter->critterId);
+
+            return;
+        }
+    }
+
+    {
+        if (*msg == "check near") {
+            auto critter = critterUnderMouse(&client->hexManager);
+            if (!critter) {
+                printf("no critter under mouse\n");
+                return;
+            }
+
+            if (playerNear(&client->hexManager, critter->critterId)) {
+                printf("near!\n");
+            } else {
+                printf("not near\n");
+            }
+
+            return;
+        }
+    }
+
+    {
+        if (*msg == "1hex") {
+            auto critter = critterUnderMouse(&client->hexManager);
+            if (!critter) {
+                return;
+            }
+
+            state->complexAction = std::make_unique<HexAttack>(critter->critterId);
+
+            return;
         }
     }
 }
